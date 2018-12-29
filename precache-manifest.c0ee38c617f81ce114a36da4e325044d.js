11a9cc9e53391d4e835148989ad7239439a07129
Victor Szeto <me@victorszeto.com>
2018-12-28 20:43:54 -0500

    Updates

---
+++ precache-manifest.c0ee38c617f81ce114a36da4e325044d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09c737c67f205aeb232f",
+    "url": "/ig-dm-archive-viewer/static/css/main.457d3fe3.chunk.css"
+  },
+  {
+    "revision": "09c737c67f205aeb232f",
+    "url": "/ig-dm-archive-viewer/static/js/main.09c737c6.chunk.js"
+  },
+  {
+    "revision": "42b32522212eaf943f89",
+    "url": "/ig-dm-archive-viewer/static/css/1.9c769a1c.chunk.css"
+  },
+  {
+    "revision": "42b32522212eaf943f89",
+    "url": "/ig-dm-archive-viewer/static/js/1.42b32522.chunk.js"
+  },
+  {
+    "revision": "23c13ff441871d1eb483",
+    "url": "/ig-dm-archive-viewer/static/js/runtime~main.23c13ff4.js"
+  },
+  {
+    "revision": "e00b72f09a37bde8d48df20922ae5f22",
+    "url": "/ig-dm-archive-viewer/index.html"
+  }
+];
